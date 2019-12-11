4efa7a7dac7f43c0298d13c33343410c36db857a
renjiejie <38006578+renjiejie@users.noreply.github.com>
2019-12-11 12:29:46 +0800

    添加谷歌的gson库

--- demo1/app/src/main/java/com/example/demo1/LoginActivity.java
+++ demo1/app/src/main/java/com/example/demo1/LoginActivity.java
@@ -11,6 +11,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import org.json.JSONObject;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -19,12 +21,29 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+
+import java.util.Iterator;
+import java.util.Map;
+
+
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 
     public void Login(View view){
@@ -63,5 +82,57 @@ public class LoginActivity extends AppCompatActivity {
         }).start();
         Intent intent = new Intent(LoginActivity.this, MainActivity.class);
         startActivity(intent);
+    }
+    private int init(String name,String pwd){
+//  192.168.3.138 这个ip地址是电脑Ipv4 地址 /20170112 是服务端的项目名称  /login/toJsonMain 是@RequestMapping的地址
+        String urlPath="http://10.0.2.2:8081/mobile/user/login";
+        //    String urlPath="http://192.168.42.207:8080/20170112/login/toJsonMain.action"; 这个是实体机(手机)的端口
+        URL url;
+        int id=0;
+        try {
+            url=new URL(urlPath);
+            JSONObject jsonObject=new JSONObject();
+            jsonObject.put("username",name);  //参数put到json串里
+            jsonObject.put("password",pwd);
+
+            //JSONObject Authorization =new JSONObject();
+            //   Authorization.put("po类名",jsonObject 即po的字段)
+
+            String content=String.valueOf(jsonObject);  //json串转string类型
+
+            HttpURLConnection conn=(HttpURLConnection) url.openConnection(); //开启连接
+            conn.setConnectTimeout(5000);
+
+            conn.setDoOutput(true);
+            conn.setRequestMethod("POST");
+            conn.setRequestProperty("ser-Agent", "Fiddler");
+            conn.setRequestProperty("Content-Type","application/json");
+            //写输出流，将要转的参数写入流里
+            OutputStream os=conn.getOutputStream();
+            os.write(content.getBytes()); //字符串写进二进流
+            os.close();
+
+            int code=conn.getResponseCode();
+            if(code==200){   //与后台交互成功返回 200
+                //读取返回的json数据
+                InputStream inputStream=conn.getInputStream();
+                // 调用自己写的NetUtils() 将流转成string类型
+                String json= NetUtils.readString(inputStream);
+
+                Gson gson=new Gson();  //引用谷歌的json包
+                User user=gson.fromJson(json,User.class); //谷歌的解析json的方法
+
+                id =user.getId();  //然后user.get你想要的值
+                String username=user.getUsername();
+                String password=user.getPassword();
+
+            }else{
+                Toast.makeText (getApplicationContext(),"数据提交失败", Toast.LENGTH_LONG ).show();
+            }
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return  id;
     }
 }
