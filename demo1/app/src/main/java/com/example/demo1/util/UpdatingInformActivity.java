563698bf23ffae008403c85b398163ce2b479fd9
1170500213 <1352487506@qq.com>
2019-12-22 01:01:28 +0800

    第一次push

---
+++ demo1/app/src/main/java/com/example/demo1/util/UpdatingInformActivity.java
@@ -0,0 +1,14 @@
+package com.example.demo1.util;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpdatingInformActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_updating_inform);
+    }
+}
