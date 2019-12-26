cca49926c23af429282db0f055ddef190a81bed1
renjiejie <38006578+renjiejie@users.noreply.github.com>
2019-12-26 12:10:36 +0800

    完成成绩后台

--- mobile/src/main/java/com/example/demo/service/TestServiceImp.java
+++ mobile/src/main/java/com/example/demo/service/TestServiceImp.java
@@ -4,26 +4,27 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.dao.TestMapper;
-
+import com.example.demo.dao.UserMapper;
 import com.example.demo.domain.Question;
 import com.example.demo.domain.Score;
 import com.example.demo.domain.Test;
-
 @Service
 public class TestServiceImp implements TestService {
 	@Autowired
 	private TestMapper testMapper;
-
 	@Override
-	public void addTest(String name, String startTime, String endTime, String courseId) {
+	public void addTest(String name, String startTime, String endTime,String courseId) {
+		// TODO Auto-generated method stub
 		testMapper.addTest(name, startTime, endTime, courseId);
-
+		
 	}
 
 	@Override
 	public List<Test> getTestList(String courseId) {
+		// TODO Auto-generated method stub
 		return testMapper.getTestList(courseId);
 	}
 
@@ -51,5 +52,5 @@ public class TestServiceImp implements TestService {
 		return testMapper.queryScore(studentId, testId);
 	}
 
-f
+
 }
